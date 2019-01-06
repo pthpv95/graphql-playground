83be8c8ef156efcb3a0173a9a63c9b3fa50ff077
Hien Pham <thehienpv@gmail.com>
2019-01-06 11:18:45 +0700

    create mutation method for graphql, using input type

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 // Scalar types: ID, String, Int, Boolean, Float
 
@@ -23,12 +24,12 @@ const users = [
   }
 ];
 
-const posts = [
+let posts = [
   {
     id: "1",
     title: "react with graphql",
     body: "xyz",
-    published: false,
+    published: true,
     author: "1"
   },
   {
@@ -47,7 +48,7 @@ const posts = [
   }
 ];
 
-const comments = [
+let comments = [
   {
     id: "1",
     post: "1",
@@ -61,13 +62,27 @@ const comments = [
     author: "3"
   }
 ];
-const typeDefs = `
+let typeDefs = `
     type Query {
       users(query: String): [User!]!
       posts(query: String): [Post!]!
       comments(query: String): [Comment!]!
       me: User!
       sum(numbers: [Float!]): Float!
+    }
+
+    type Mutation{
+      createUser(data: createUserInput): User!
+      createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+      createComment(post: String!, content: String!, author: String): Comment!
+      deleteUser(id: String!): User!
+    }
+
+    # input type
+    input createUserInput{
+      name: String!
+      email: String!
+      age: Int
     }
 
     type User{
@@ -126,9 +141,82 @@ const resolvers = {
       return comments;
     }
   },
+  Mutation: {
+    createUser(parent, agrs, ctx, info) {
+      const emailTaken = users.some(u => u.email === agrs.data.email);
+      if (emailTaken) {
+        throw new Error("Email has taken");
+      }
+
+      const user = {
+        id: uuidv4(),
+        ...agrs.data
+      };
+
+      users.push(user);
+      return user;
+    },
+    deleteUser(parent, agrs) {
+      const userIndex = users.findIndex(x => x.id === agrs.id);
+
+      if (userIndex === -1) {
+        throw new Error("User not existed");
+      }
+
+      const deletedUser = users.splice(userIndex, 1);
+      posts = psots.filter(c => {
+        const match = c.author === agrs.id;
+
+        if (match) {
+          comments = comments.filter(c => c.post !== c.id);
+        }
+
+        return !match;
+      });
+      comments = comments.filter(p => p.author !== agrs.id);
+
+      return deletedUser[0];
+    },
+    createPost(parent, agrs, ctx, info) {
+      const existedUser = users.some(u => u.id === agrs.author);
+      if (!existedUser) {
+        throw new Error("User not existed");
+      }
+
+      const post = {
+        id: uuidv4(),
+        ...agrs
+      };
+
+      posts.push(post);
+      return post;
+    },
+    createComment(parent, agrs) {
+      const existedUser = users.some(u => u.id === agrs.author);
+      if (!existedUser) {
+        throw new Error("User not existed");
+      }
+
+      const postExists = posts.some(
+        p => p.id === agrs.post && p.published === true
+      );
+
+      if (!postExists) {
+        throw new Error("Post not existed");
+      }
+
+      const comment = {
+        id: uuidv4(),
+        ...agrs
+      };
+
+      comments.push(comment);
+      return comment;
+    }
+  },
   Post: {
     author(parent, argx, ctx, info) {
-      return users.find(x => x.id === parent.id);
+      return users.find(x => x.id === parent.author);
     },
     comments(parent) {
       return comments.filter(x => x.post === parent.id);
